abcbcbcbdb8ea31440ba8c945c8109a6480ea0ee
Alfred Matrimonio <30339882+pinoynukerz@users.noreply.github.com>
2022-10-26 08:47:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,44 +7,44 @@
 //    require 'PHPMailer/src/PHPMailer.php';
 //    require 'PHPMailer/src/SMTP.php';
     
-if(isset($_POST['submit'])){
+// if(isset($_POST['submit'])){
    
-    $name    = $_POST['fullname'];
-    $email   = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+   // $name    = $_POST['fullname'];
+   // $email   = $_POST['email'];
+   // $subject = $_POST['subject'];
+   // $message = $_POST['message'];
 
     
-    echo '<div style="margin-top:45px margin-left:40px;margin-right:auto;">';
+   // echo '<div style="margin-top:45px margin-left:40px;margin-right:auto;">';
     //    echo $_POST['fullname'].''.$_POST['email'].''.$_POST['subject'].''.$_POST['message'];
     
     //email
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->SMTPDebug = 0;
-    $mail->Host = 'email-smtp.us-east-1.amazonaws.com' //AWS ses 
-    $mail->Port = 465;
-    $mail->SMTPSecure = 'tls';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'AKIAVFEKWWJKOCX3NVOC';
-    $mail->Password = 'BNHBX65+DorQDmmi9T95DCei5oH6KM6M1umgyt4Pht4i';
-    $mail->setFrom('jmattz23@hotmail.com','Info');
-    $mail->addAddress($email,$name);
+    // $mail = new PHPMailer;
+    // $mail->isSMTP();
+    // $mail->SMTPDebug = 0;
+    // $mail->Host = 'email-smtp.us-east-1.amazonaws.com' //AWS ses 
+   //  $mail->Port = 465;
+    // $mail->SMTPSecure = 'tls';
+    // $mail->SMTPAuth = true;
+    // $mail->Username = 'AKIAVFEKWWJKOCX3NVOC';
+    // $mail->Password = 'BNHBX65+DorQDmmi9T95DCei5oH6KM6M1umgyt4Pht4i';
+    // $mail->setFrom('jmattz23@hotmail.com','Info');
+    // $mail->addAddress($email,$name);
     //$mail->addCC('email id','name')
-    $mail->Subject = $subject;
-    $mail->msgHTML($message);
-    $mail->AltBody = 'HTML does not Support';
+    // $mail->Subject = $subject;
+    // $mail->msgHTML($message);
+    // $mail->AltBody = 'HTML does not Support';
 //    $mail->addAttachment('image.jpg');
   
-    if(!$mail->send()){
-        echo "Error".$mail->ErrorInfo;
+    // if(!$mail->send()){
+    //     echo "Error".$mail->ErrorInfo;
     
-    }else{
-        echo "Mail Sent";
-    }
+    // }else{
+    //     echo "Mail Sent";
+    // }
     
-    echo '</div>';
-}
+    // echo '</div>';
+// }
 
 
 ?>
@@ -62,4 +62,3 @@ if(isset($_POST['submit'])){
     <input type="submit" name="submit" />
 </form>
 </div>
-
