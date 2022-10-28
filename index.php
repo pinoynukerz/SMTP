67e17e3a68c09917d68046b2ec6f7d107c57beca
Alfred Matrimonio <30339882+pinoynukerz@users.noreply.github.com>
2022-10-28 09:33:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,39 +14,54 @@
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
+    $sender = 'jmattz23@hotmail.com';
+    $senderName = 'Sender Name';
+    $recipient = 'alfred.mattz@gmail.com';
+    $usernameSmtp = 'AKIAVFEKWWJKDDLUBNRU'; 
+    $passwordSmtp = 'BEWEfDgnPyD6GB9nh85C0QMPsxIg1ykA8uMpGgtUdGsz';
+    $host = 'email-smtp.us-east-1.amazonaws.com';
+    $port = 465;
+     
+    $subject  = 'Inquiry: by $email ';
+    $bodyText = '$subject';
+    $bodyHtml = '$message';
+     
+     
+    $mail = new PHPMailer(true);
+
+try {
+    // Specify the SMTP settings.
+    $mail->isSMTP(true);
+    $mail->setFrom($sender, $senderName);
+    $mail->Username   = $usernameSmtp;
+    $mail->Password   = $passwordSmtp;
+    $mail->Host       = $host;
+    $mail->Port       = $port;
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = 'ssl';
+    
+    $mail->addAddress($recipient);
+    
+    $mail->isHTML(true);
+    $mail->Subject    = $subject;
+    $mail->Body       = $bodyHtml;
+    $mail->AltBody    = $bodyText;
+    
+    $mail->Send();
+    
+    echo "Email sent!" , PHP_EOL;
+} catch (phpmailerException $e) {
+    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+} catch (Exception $e) {
+    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+}
+     
     
    echo '<div style="margin-top:45px margin-left:40px;margin-right:auto;">';
 
 
-
-    //email
-     $mail = new PHPMailer(true);
-     $mail->isSMTP(true);
-     $mail->SMTPDebug = 0;
-     $mail->Host = 'email-smtp.us-east-1.amazonaws.com' 
-     $mail->Port = 465;
-     $mail->SMTPSecure = 'ssl';
-     $mail->SMTPAuth = true;
-     $mail->Username = 'AKIAVFEKWWJKDDLUBNRU';
-     $mail->Password = 'BEWEfDgnPyD6GB9nh85C0QMPsxIg1ykA8uMpGgtUdGsz';
-     $mail->setFrom('jmattz23@hotmail.com','Info');
-     $mail->addAddress($email,$name);
-   //$mail->addCC('email id','name');
-     $mail->Subject = $subject;
-     $mail->msgHTML($message);
-     $mail->AltBody = 'HTML does not Support';
-   //$mail->addAttachment('image.jpg');
-
-     
     
-    if(!$mail->send()){
-      echo "Error".$mail->ErrorInfo;
-    
-     }else{
-         echo "Mail Sent";
-     }
-    
-     echo '</div>';
+   echo '</div>';
  }
 ?>
 
@@ -55,12 +70,15 @@
 </head>
 
 <div style="width:50%; margin-top:45px; margin-left:40px; margin-right:auto;">
+    
+<h2>Contact Us</h2>
 <form action="" method="POST">
-    <p>name: <input type="text" name="fullname" /> </p>
-    <p>email: <input type="email" name="email" /> </p>
+    <p>name:    <input type="text" name="fullname" /> </p>
+    <p>email:   <input type="email" name="email" /> </p>
     <p>subject: <input type="subject" name="subject" /> </p>
-    <p>message <textarea name="message"></textarea></p>
-    <input type="submit" name="submit" />
+    <p>message  <textarea name="message"></textarea></p>
+                <input type="submit" name="submit" />
 </form>
+    
 </div>
 
