5c6f6b90020212b8bfb14e54cd088826511f4c34
Alfred Matrimonio <30339882+pinoynukerz@users.noreply.github.com>
2022-10-28 11:56:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 //SMTP MAIL
+
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     
@@ -8,12 +9,14 @@
     require 'PHPMailer/src/SMTP.php';
     
  if(isset($_POST['submit'])){
+     
+    $datetoday     =  date("l jS F Y g:ia"); 
 
     echo '<div style="margin-top:45px margin-left:40px;margin-right:auto;">';
 
     echo $_POST['fullname'].''.$_POST['email'].''.$_POST['subject'].''.$_POST['message'];
     
-   echo '</div>';
+    echo '</div>';
    
     $name    = $_POST['fullname'];
     $email   = $_POST['email'];
@@ -25,16 +28,16 @@
 </div><div style=\"background-color:#f6f6f6;padding:30px\"><div style=\"margin:0 auto;width:620px;background-color:#fff;border:1px solid #eee;padding:30px\">
     <table width=\"100%\" cellpadding=\"4\" cellspacing=\"0\" style=\"border-collapse:collapse;font-size:12px;font-family:verdana;\" border=\"1\" bordercolor=\"#CCCCC\">
     <tr><td bgcolor=\"#f4f4f4\" align=\"right\">Date</td><td >$datetoday</td></tr>
-    <tr><td width=\"150\" bgcolor=\"#f4f4f4\" align=\"right\">Name</td><td width=\"450\"> $full_name </td></tr>
-    <tr><td bgcolor=\"#f4f4f4\" align=\"right\">Email</td><td > $from </td></tr>
-    <tr><td bgcolor=\"#f4f4f4\" align=\"right\">Telephone</td><td >$phone </td></tr>
+    <tr><td width=\"150\" bgcolor=\"#f4f4f4\" align=\"right\">Name</td><td width=\"450\"> $name </td></tr>
+    <tr><td bgcolor=\"#f4f4f4\" align=\"right\">Email</td><td > $email </td></tr>
+    <tr><td bgcolor=\"#f4f4f4\" align=\"right\">Subject</td><td >$subject </td></tr>
     <tr><td bgcolor=\"#f4f4f4\" align=\"right\">IP Address</td><td > $ip_address </td></tr>
     <tr><td bgcolor=\"#f4f4f4\" colspan=\"2\"><FONT SIZE=\"+1\" >Message was Sent via Contact us page</FONT></td></tr>
     <tr><td bgcolor=\"#f4f4f4\" valign=\"top\" align=\"right\" height=\"150\">Message</td><td valign=\"top\" > $messagepost</td></tr>
     </table></div></div><div style=\"padding-top:30px;text-align:center;padding-bottom:30px;font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif\">
     <div style=\"width:640px;background-color:#ffffff;margin:0 auto\">
     <p style=\"margin:0 0 10px\">Copyright © 2022 , All rights reserved.</p>
-    <p style=\"margin:0 0 10px\">You are receiving this email because you are subscribed to <a style=\"color:#00aeef;text-decoration:none\" href=\"http://condo.com.ph\" target=\"_blank\">Condo.Com.Ph</a></p>
+    <p style=\"margin:0 0 10px\">You are receiving this email because you are subscribed to <a style=\"color:#00aeef;text-decoration:none\" href=\"http://condo.com.ph\" target=\"_blank\">Audiophile.Ph</a></p>
     <div class=\"yj6qo\"></div></div><div class=\"adL\"></div></div>"; 
      
 
